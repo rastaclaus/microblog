ac80ef9c63b1da509f9cf27e775c583f8c856509
Artem Shestopalov <ashestopalov@aorti.ru>
2020-02-12 04:29:24 +0300

    end chapter3

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,11 @@
-from flask import render_template
+from flask import(
+        render_template,
+        flash,
+        redirect,
+        url_for)
+
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -14,3 +20,12 @@ def index():
          'body': 'Какая гадость эта ваша заливная рыба!!'}]
     context = {'user': user, 'posts': posts, 'title': 'Home'}
     return render_template('index.html', **context)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
