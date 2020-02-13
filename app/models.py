03f08afd4465dc619983c6749c6fbb6af13d2b84
Шестопалов Артём Алексеевич <a.a.shestopalov@sozvezdie.su>
2020-02-13 21:36:05 +0300

    add register form (chapt5 done)

--- app/models.py
+++ app/models.py
@@ -6,6 +6,10 @@ from flask_login import UserMixin
 from app import db
 from app import login
 
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -31,7 +35,3 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'<Post {self.body}>'
-
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,8 @@ from flask import(
         render_template,
         flash,
         redirect,
-        url_for)
+        url_for,
+        request)
 from flask_login import (
         current_user,
         login_user,
@@ -11,15 +12,14 @@ from flask_login import (
         login_required)
 
 
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 
 @app.route('/')
 @app.route('/index')
 @login_required
 def index():
-    user = {'username': 'artems'}
     posts = [
         {'author': {'username': 'John'},
          'body': 'Beautiful day in Portland!'},
@@ -27,7 +27,7 @@ def index():
          'body': 'The Avengers movie was so cool!'},
         {'author': {'username': 'Ипполит'},
          'body': 'Какая гадость эта ваша заливная рыба!!'}]
-    context = {'user': user, 'posts': posts, 'title': 'Home'}
+    context = {'posts': posts, 'title': 'Home Page'}
     return render_template('index.html', **context)
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -37,8 +37,8 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_passwor(form.password.data):
-            flash("Invalid username or password") 
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password")
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
@@ -52,3 +52,18 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data,
+                    email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
