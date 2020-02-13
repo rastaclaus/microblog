ba479dde0842083591d62e21c6c0ebba0637e7cb
Шестопалов Артём Алексеевич <a.a.shestopalov@sozvezdie.su>
2020-02-13 14:22:12 +0300

    config database uri

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
 import os
+from pathlib import Path
+
+basedir = Path(__file__).parent.absolute()
 
 class Config:
     SECRET_KEY = 'you-will-newer-guess'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+            f'sqlite:/{ basedir / "app.db" }'
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
