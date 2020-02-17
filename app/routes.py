4dbc06966cfde4513ab10066e317dd37f7991e90
Artem Shestopalov <ashestopalov@aorti.ru>
2020-02-18 01:02:21 +0300

    tests and following

--- app/routes.py
+++ app/routes.py
@@ -98,4 +98,33 @@ def edit_profile():
         title="Edit Profile",
         form=form)
 
+@app.route("/follow/<username>")
+@login_required
+def follow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash(f'User {usename} not found')
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You cannot follow yourself!')
+        return redirect(url_for('user', usename=username))
+    current_user.follow(user)
+    db.session.commit()
+    flash(f'You are foloowing to {username}!')
+    return redirect(url_for('user', username=username))
 
+
+@app.route("/unfollow/<username>")
+@login_required
+def unfollow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash(f'User {usename} not found')
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You cannot unfollow yourself!')
+        return redirect(url_for('user', usename=username))
+    current_user.unfollow(user)
+    db.session.commit()
+    flash(f'You are not foloowing to {username}!')
+    return redirect(url_for('user', username=username))
