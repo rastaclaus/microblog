0d8d1dac0563933a43ebfa6b46581cddf2d01101
Artem Shestopalov <ashestopalov@aorti.ru>
2020-02-19 00:44:25 +0300

    bootstrap templates

--- app/__init__.py
+++ app/__init__.py
@@ -8,17 +8,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
-
-
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 login = LoginManager(app)
 login.login_view = 'login'
-
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
--- run_debug.sh
+++ run_debug.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
-
+source flask.env
+export MAIL_SERVER=smtp.gmail.com
+export MAIL_PORT=587
+export MAIL_USE_TLS=1
 export FLASK_APP=microblog.py
 export FLASK_ENV=development
 flask run
