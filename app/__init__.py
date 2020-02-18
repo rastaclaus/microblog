c60f08b275bf12d1f06a9baa48612f45973e5231
Artem Shestopalov <ashestopalov@aorti.ru>
2020-02-19 01:08:10 +0300

    moment.js

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -16,6 +17,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 login = LoginManager(app)
 login.login_view = 'login'
 
@@ -53,7 +55,6 @@ if not app.debug:
     app.logger.setLevel(logging.INFO)
 
     app.logger.info("Microblog startup")
-
 
 
 from app import routes, models, errors
