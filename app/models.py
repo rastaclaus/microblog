6c7b650dab0014cb9fb842ec3674119545c1af53
Artem Shestopalov <ashestopalov@aorti.ru>
2020-02-18 23:40:24 +0300

    password reset functional

--- app/models.py
+++ app/models.py
@@ -1,10 +1,12 @@
 from datetime import datetime
+from time import time
 from hashlib import md5
 
+import jwt
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
-from app import db
+from app import app, db
 from app import login
 
 @login.user_loader
@@ -62,6 +64,23 @@ class User(UserMixin, db.Model):
                 followers.c.follower_id == self.id)
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(
+                token,
+                app.config['SECRET_KEY'],
+                algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
+
 
 
 class Post(db.Model):
--- config.py
+++ config.py
@@ -13,5 +13,5 @@ class Config:
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['tema.rastaclaus@gmail.com']
+    ADMINS = ['develop.tema.rastaclaus@gmail.com', 'tema.rastaclaus@gmail.com']
     POSTS_PER_PAGE = 10
