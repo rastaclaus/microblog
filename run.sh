91ce19b05c3c27f34dd5e84e7cfaf6d6e9e95788
Artem Shestopalov <ashestopalov@aorti.ru>
2020-02-18 15:44:18 +0300

    mail settings

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
-
+source flask.env
+export MAIL_SERVER=smtp.gmail.com
+export MAIL_PORT=587
+export MAIL_USE_TLS=1
 export FLASK_APP=microblog.py
-# export MAIL_SERVER=localhost
-# export MAIL_PORT=8025
 flask run
